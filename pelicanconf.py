88e10fc9b48457e9b84ac075dc0427cbdd84d156
Cees van de Griend <cees@griend.eu>
2022-09-23 11:08:16 +0200

    start2run 5

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,12 @@
-AUTHOR = 'Cees van de Griend'
-SITENAME = 'Blog Cees van de Griend'
-SITEURL = ''
+AUTHOR = "Cees van de Griend"
+SITENAME = "Blog Cees van de Griend"
+SITEURL = ""
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'Europe/Amsterdam'
+TIMEZONE = "Europe/Amsterdam"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -16,16 +16,16 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ("griend", "https://www.griend.eu/"),
+    ("xantara-it", "https://www.xantara-it.nl/"),
+)
 
 # Social widget
 SOCIAL = (
-          ('GitHub', 'https://github.com/ceesvandegriend'),
-          ('Strava', 'https://www.strava.com/athletes/cvdgriend'),
-        )
+    ("GitHub", "https://github.com/ceesvandegriend"),
+    ("Strava", "https://www.strava.com/athletes/cvdgriend"),
+)
 
 DEFAULT_PAGINATION = 10
 
